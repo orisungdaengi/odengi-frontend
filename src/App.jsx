c3bd2436efbd07ef6bd0b8561d053d754a262589
hobaksweetpotato <164849940+hobaksweetpotato@users.noreply.github.com>
2025-08-19 18:40:37 +0900

    init: Vite+React, Tailwind, fixed 402x874 stage
    # Conflicts:
    #	.gitignore
    #	README.md

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './FixedStage.css'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function App() {
+    return (
+        <div className="stage">
+            <div className="safe">
+                {/* 여기에 전부 */}
+            </div>
+        </div>
+    )
+}
+
